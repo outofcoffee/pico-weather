92b8057ea1b67572cc805f9aaa399fa25f6782f8
Pete Cornish <outofcoffee@gmail.com>
2024-01-01 00:15:28 +0000

    feat: adds weather API call.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import machine
 import utime
 import network
+import urequests as requests
 
 from display import EPD_2in13_V3_Landscape
 
@@ -24,7 +25,6 @@ def read_config():
     return ssid, password, lat, lon, openweathermap_key
 
 
-
 def connect_to_network() -> str:
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
@@ -41,7 +41,7 @@ def connect_to_network() -> str:
     return ip_addr
 
 
-def display_info(line1: str, line2: str, line3: str, line4: str):
+def display_info(line1: str, line2: str = "", line3: str = "", line4: str = ""):
     epd.Clear()
 
     epd.fill(0xff)
@@ -50,8 +50,11 @@ def display_info(line1: str, line2: str, line3: str, line4: str):
     epd.text(line3, 0, 30, 0x00)
     epd.text(line4, 0, 40, 0x00)
     epd.display(epd.buffer)
-    epd.delay_ms(2000)
+    epd.delay_ms(150)
 
+
+def display_additional():
+    epd.delay_ms(2000)
     epd.vline(5, 55, 60, 0x00)
     epd.vline(100, 55, 60, 0x00)
     epd.hline(5, 55, 95, 0x00)
@@ -79,20 +82,46 @@ def display_info(line1: str, line2: str, line3: str, line4: str):
     epd.sleep()
 
 
+def fetch_weather() -> str:
+    url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={openweathermap_key}"
+    print(f"querying {url}")
+    r = requests.get(url)
+    resp: dict = r.json()
+    print(resp)
+    r.close()
+
+    current_conditions = resp['current']
+
+    # convert from K to C
+    temp = current_conditions['temp'] -273.15
+    current_weather = current_conditions['weather']
+
+    summary = f"{current_weather['main']} ({temp}) {current_weather['description']}"
+    print(summary)
+    return summary
+
+
 if __name__ == '__main__':
     ssid, password, lat, lon, openweathermap_key = read_config()
 
     epd = EPD_2in13_V3_Landscape()
-    epd.Clear()
 
-    epd.fill(0xff)
-    epd.text(f"Connecting to {ssid}...", 0, 10, 0x00)
-    epd.display(epd.buffer)
+    display_info(f"Connecting to {ssid}...")
 
-    ip: str
     try:
         ip = connect_to_network()
     except KeyboardInterrupt:
+        print('received keyboard interrupt when connecting to network')
         machine.reset()
 
     display_info("Connected to", ssid, "IP:", ip)
+
+    try:
+        weather = fetch_weather()
+    except KeyboardInterrupt:
+        print('received keyboard interrupt when fetching weather')
+        machine.reset()
+
+    display_info("Weather", weather, "", "")
+
+    display_additional()
