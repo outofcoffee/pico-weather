cf5d4218e5b20da3449fcc49edb1235d05e671ab
Pete Cornish <outofcoffee@gmail.com>
2024-01-05 23:33:07 +0000

    refactor: moves additional eink examples to attic.

---
+++ attic/display_additional.py
@@ -0,0 +1,23 @@
+
+def display_additional(epd):
+    epd.delay_ms(2000)
+    epd.vline(5, 55, 60, 0x00)
+    epd.vline(100, 55, 60, 0x00)
+    epd.hline(5, 55, 95, 0x00)
+    epd.hline(5, 115, 95, 0x00)
+    epd.line(5, 55, 100, 115, 0x00)
+    epd.line(100, 55, 5, 115, 0x00)
+    epd.display(epd.buffer)
+    epd.delay_ms(2000)
+
+    epd.rect(130, 10, 40, 80, 0x00)
+    epd.fill_rect(190, 10, 40, 80, 0x00)
+    epd.Display_Base(epd.buffer)
+    epd.delay_ms(2000)
+
+    epd.init()
+    for i in range(0, 10):
+        epd.fill_rect(175, 105, 10, 10, 0xff)
+        epd.text(str(i), 177, 106, 0x00)
+        epd.display_Partial(epd.buffer)
+    print("sleep")
