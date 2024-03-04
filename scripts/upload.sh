e2c3472a5c89fe8b33cde141244e07cabc6b6b0f
Pete Cornish <outofcoffee@gmail.com>
2024-03-04 01:31:18 +0000

    build: allows files to be passed to upload convenience script.

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,8 +4,12 @@ DEVICE_PORT=$( ls /dev/cu.usbmodem* )
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
-FILES_TO_UPLOAD=$(ls *.py)
-FILES_TO_UPLOAD+=" config.txt"
+if [[ $# -eq 0 ]]; then
+  FILES_TO_UPLOAD=$(ls *.py)
+  FILES_TO_UPLOAD+=" config.txt"
+else
+  FILES_TO_UPLOAD="$@"
+fi
 
 python3 ./scripts/microupload.py \
   -v \
